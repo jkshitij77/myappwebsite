e37d35012cf2fc9baef9dcc6046ba72bf44dbde9
Kshitij Jain <kshitij.jain.23@dartmouth.edu>
2020-04-14 06:41:01 +0530

    Updates

---
+++ precache-manifest.ec2efb1162d6737e85960b7bd8a5e05a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa193b995932e3a5c83ba035737079d",
+    "url": "/myappwebsite/index.html"
+  },
+  {
+    "revision": "aefd35443b20fcbe731e",
+    "url": "/myappwebsite/static/css/main.65731573.chunk.css"
+  },
+  {
+    "revision": "e79533d813b9960ae205",
+    "url": "/myappwebsite/static/js/2.2d64ed97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myappwebsite/static/js/2.2d64ed97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefd35443b20fcbe731e",
+    "url": "/myappwebsite/static/js/main.2959a02d.chunk.js"
+  },
+  {
+    "revision": "3962a4c67ad47e1eb8b8",
+    "url": "/myappwebsite/static/js/runtime-main.a3a8d997.js"
+  },
+  {
+    "revision": "bf98fb52ccf5d04c8eefecfe2f60373c",
+    "url": "/myappwebsite/static/media/AOT.bf98fb52.jpg"
+  },
+  {
+    "revision": "c65fd3ef522d468b6050be1ee6ee6f2f",
+    "url": "/myappwebsite/static/media/Android.c65fd3ef.png"
+  },
+  {
+    "revision": "1949dfd925dd06c221ce6cea3d2a8d6b",
+    "url": "/myappwebsite/static/media/Berk.1949dfd9.png"
+  },
+  {
+    "revision": "3e4a7bd55234a0726b8afae8641752bf",
+    "url": "/myappwebsite/static/media/frisbee.3e4a7bd5.jpg"
+  },
+  {
+    "revision": "ab102ed36f6a89e79e7fbb2dd2fc08b0",
+    "url": "/myappwebsite/static/media/javacode.ab102ed3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myappwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
